3e93b508d97e993132702b5ef982550d32b2c2a1
houjie <18817958165@163.com>
2023-06-30 21:44:32 +0800

    demo for demo01 6-30

--- learn_demo/demo01/app.py
+++ learn_demo/demo01/app.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask,request
 
 
 app = Flask(__name__)
@@ -9,10 +9,26 @@ app = Flask(__name__)
 def hello_world():
     return 'hello world!!!'
 
-# 1. 修改host = 0。0。0。0  就能其他电脑访问
+# 1. 修改host = 0.0.0.0  就能其他电脑访问
 # --host=0.0.0.0
 
 # 2.修改端口号
+
+# 传参数的路径   将参数固定到path中
+@app.route('/blog/<string:blog_id>')
+def blog_detail(blog_id):
+    return '访问的是 - > %s' % blog_id
+
+# 查询字符串的方式传参
+# /book/list  默认返回第一页
+# /book/list?page=2 获取第二页
+@app.route('/book/list') # 需要引用全局对象
+def book_list():
+    # request.args 类字段的
+    page = request.args.get('page',default=1,type=int)
+    return f'获取的是第{page}的book_list'
+
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=2000)
