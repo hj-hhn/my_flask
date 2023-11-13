876497ebbf06538168ba55676bc7013419662a18
houjie <18817958165@163.com>
2023-11-13 21:47:27 +0800

    flask 8-31 demo

--- learn_demo/demo02/blueprints/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin python3
-# -*- encoding: utf-8 -*-
-# @Author : jie.hou
-# @File : __init__.py.py
-# @Time : 2023/7/5 23:07
--- learn_demo/lesson/test.py
+++ learn_demo/lesson/test.py
@@ -1,2 +1,21 @@
 # -*- coding: utf-8 -*-
-print('hello') # ???
+import pymysql
+
+conn = pymysql.connect(
+    host='localhost',
+    port=3307,
+    user='root',
+    password='root',
+    database='mysql'
+)
+
+
+cursor= conn.cursor()
+
+
+cursor.execute("select host,user from user")
+
+print(cursor.fetchall())
+
+
+cursor.close()
